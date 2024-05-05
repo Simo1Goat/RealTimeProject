69b86f2cdc0ebe39f133510fe13f2ec859356e64
mhabibi <mhabibi@leyton.com>
2024-05-05 23:55:38 +0100

    update: push data into the broker each minute

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -10,9 +10,23 @@ from airflow.operators.python import PythonOperator
 
 
 def stream_data():
-    response = get_data(method="GET")
-    formatted_data = format_data(response)
-    return json.dumps(formatted_data, indent=3)
+    producer = KafkaProducer(bootstrap_servers=KAFKA_HOST, max_block_ms=5000)
+    curr_time = time.time()
+    while True:
+        try:
+            # push the data each minute to the broker
+            if time.time() > curr_time + 60:
+                response = get_data()
+                formatted_data = format_data(response)
+                producer.send(topic="users", value=json.dumps(formatted_data).encode("utf-8"))
+                logging.info(msg="The message is sent successfully")
+                curr_time = time.time()
+            else:
+                logging.info(msg="waiting ...")
+                time.sleep(30)
+        except Exception as err:
+            logging.error(msg="An Error occurred during fetching data, details {}".format(err))
+            continue
 
 
 default_args = {
