ea762f7fc2d8338889b4d7db636cb295aaaf6fdd
mhabibi <mhabibi@leyton.com>
2024-05-05 23:53:57 +0100

    feat: create airflow dag and task to stream the data into the broker

---
+++ dags/config.py
@@ -0,0 +1,3 @@
+import os
+API_URL = "https://randomuser.me/api/"
+KAFKA_HOST = os.getenv("KAFKA_HOST", "broker:29092")
--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,33 +1,12 @@
 import json
-import requests
-from requests.exceptions import HTTPError
-from config import API_URL
-
-
-def get_data(method="GET", retrial=0):
-    response = None
-    if retrial >= 4:
-        print("Reached max retrial finishing the process ...")
-        return response
-    try:
-        params = {
-            "page": 1,
-            "results": 1
-        }
-        response = requests.request(method=method, url=API_URL, params=params)
-        response.raise_for_status()
-    except HTTPError as e:
-        if response.status_code == 405:
-            print("Method is not allowed, Retry different HTTP method ...")
-            retrial += 1
-            get_data(method="POST", retrial=retrial)
-        else:
-            print("Http Error occurred, details {}".format(e))
-    except Exception as e:
-        print("An Error occurred, details {}".format(e))
-    else:
-        # handle successful response
-        return response.json()["results"][0]
+import time
+import logging
+from airflow import DAG
+from config import KAFKA_HOST
+from datetime import datetime
+from kafka import KafkaProducer
+from utils.utils import get_data, format_data
+from airflow.operators.python import PythonOperator
 
 
 def stream_data():
@@ -36,20 +15,17 @@ def stream_data():
     return json.dumps(formatted_data, indent=3)
 
 
-def format_data(res):
-    location = res["location"]
-    data = {
-        "first_name": res["name"]["first"],
-        "last_name": res["name"]["last"],
-        "gender": res["gender"],
-        "address": f'{location["street"]["number"]} {location["street"]["name"]}, '
-                   f'{location["city"]}, {location["state"]}, {location["country"]}',
-        "postcode": location["postcode"],
-        "email": res["email"],
-        "username": res["login"]["username"],
-        "dob": res["dob"]["date"],
-        "registered_data": res["registered"]["date"],
-        "phone": res["phone"],
-        "picture": res["picture"]["medium"]
-    }
-    return data
+default_args = {
+    "owner": "mhabibi",
+    "start_date": datetime(2024, 1, 1)
+}
+
+with DAG(dag_id="user_automation",
+         default_args=default_args,
+         schedule_interval="@daily",
+         catchup=False) as dag:
+
+    streaming_task = PythonOperator(
+        task_id="stream_data_from_api",
+        python_callable=stream_data
+    )
