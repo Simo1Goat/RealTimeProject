61dfc26441825f64acb94316401905067fad379c
mhabibi <mhabibi@leyton.com>
2024-05-06 14:16:44 +0100

    update: install requirements after launching the services of airflow

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [ -e "/opt/airflow/requirements.txt" ]; then
+  $(command -v pip) install -r requirements.txt
+fi
+
 if [ ! -f "/opt/airflow/airflow.db" ]; then
   airflow db init && \
   airflow users create --username admin --firstname admin --lastname admin --role Admin --email admin@example.com --password admin
